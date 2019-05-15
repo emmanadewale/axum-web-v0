4e8f4d915ae353f4c2373900578ef5e71d951123
emmanadewale <50661107+emmanadewale@users.noreply.github.com>
2019-05-15 05:44:32 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,143 @@
+(function($) {
+  
+  "use strict";  
+
+  $(window).on('load', function() {
+
+    /* 
+   MixitUp
+   ========================================================================== */
+  $('#portfolio').mixItUp();
+
+  /* 
+   One Page Navigation & wow js
+   ========================================================================== */
+    var OnePNav = $('.onepage-nev');
+    var top_offset = OnePNav.height() - -0;
+    OnePNav.onePageNav({
+      currentClass: 'active',
+      scrollOffset: top_offset,
+    });
+  
+  /*Page Loader active
+    ========================================================*/
+    $('#preloader').fadeOut();
+
+  // Sticky Nav
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 200) {
+            $('.scrolling-navbar').addClass('top-nav-collapse');
+        } else {
+            $('.scrolling-navbar').removeClass('top-nav-collapse');
+        }
+    });
+
+    /* slicknav mobile menu active  */
+    $('.mobile-menu').slicknav({
+        prependTo: '.navbar-header',
+        parentTag: 'liner',
+        allowParentLinks: true,
+        duplicate: true,
+        label: '',
+        closedSymbol: '<i class="icon-arrow-right"></i>',
+        openedSymbol: '<i class="icon-arrow-down"></i>',
+      });
+
+      /* WOW Scroll Spy
+    ========================================================*/
+     var wow = new WOW({
+      //disabled for mobile
+        mobile: false
+    });
+
+    wow.init();
+
+    /* Nivo Lightbox 
+    ========================================================*/
+    $('.lightbox').nivoLightbox({
+        effect: 'fadeScale',
+        keyboardNav: true,
+      });
+
+    /* Counter
+    ========================================================*/
+    $('.counterUp').counterUp({
+     delay: 10,
+     time: 1000
+    });
+
+
+    /* Back Top Link active
+    ========================================================*/
+      var offset = 200;
+      var duration = 500;
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > offset) {
+          $('.back-to-top').fadeIn(400);
+        } else {
+          $('.back-to-top').fadeOut(400);
+        }
+      });
+
+      $('.back-to-top').on('click',function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: 0
+        }, 600);
+        return false;
+      });
+
+	$("#news-slider4").owlCarousel({
+        items:3,
+        itemsDesktop:[1199,3],
+        itemsDesktopSmall:[1000,2],
+        itemsMobile:[600,1],
+        pagination:false,
+        navigationText:false,
+        autoPlay:true
+    	});
+    
+
+
+
+   wow = new WOW(
+      {
+        animateClass: 'animated',
+        offset:       100,
+        callback:     function(box) {
+          console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+        }
+      }
+    );
+    wow.init();
+    document.getElementById('moar').onclick = function() {
+      var section = document.createElement('section');
+      section.className = 'section--purple wow fadeInDown';
+      this.parentNode.insertBefore(section, this);
+    };
+
+
+
+
+
+	$('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
+ 	var next = $(this).next();
+  	if (!next.length) {
+    		next = $(this).siblings(':first');
+  	}
+  	next.children(':first-child').clone().appendTo($(this));
+
+  	for (var i=0;i<4;i++) {
+    		next=next.next();
+    		if (!next.length) {
+      		 next=$(this).siblings(':first');
+    	}
+    	next.children(':first-child').clone().appendTo($(this));
+  	}
+	});
+
+
+
+  });      
+
+}(jQuery));
